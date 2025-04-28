483b25deb0c0cecfdfd32fb4a25a06dac6ed0ad0
rhinzz <prama.sudjana@gmail.com>
2025-04-28 10:12:57 +0700

    Modify script so the first item is active

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
 accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", event => {
+    accordionItemHeader.addEventListener("click", () => {
+        const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+
+        if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+            currentlyActiveAccordionItemHeader.classList.toggle("active");
+            currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        }
+
         accordionItemHeader.classList.toggle("active");
         const accordionItemBody = accordionItemHeader.nextElementSibling;
 
@@ -12,3 +19,10 @@ accordionItemHeaders.forEach(accordionItemHeader => {
         }
     });
 });
+
+const firstAccordionItemHeader = accordionItemHeaders[0];
+if (firstAccordionItemHeader) {
+    firstAccordionItemHeader.classList.add("active");
+    const firstAccordionItemBody = firstAccordionItemHeader.nextElementSibling;
+    firstAccordionItemBody.style.maxHeight = firstAccordionItemBody.scrollHeight + "px";
+}
